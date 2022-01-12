652200dd5f2db1753401cf2021e6ab0353130f37
Matt Filetto <matt.filetto@gmail.com>
2022-01-12 06:41:09 -0800

    gs101: remove usbradio debugging on userdebug builds
    * Causes adb to not initalize until after BootCompleted
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: Ibe89d40a5fe88f832b35ed7fe48bb34432f89ca6

--- device.mk
+++ device.mk
@@ -374,10 +374,10 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
 # default usb debug functions
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-PRODUCT_PROPERTY_OVERRIDES += \
-	persist.vendor.usb.usbradio.config=dm
-endif
+#ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+#PRODUCT_PROPERTY_OVERRIDES += \
+#	persist.vendor.usb.usbradio.config=dm
+#endif
 
 # Power HAL
 PRODUCT_COPY_FILES += \
