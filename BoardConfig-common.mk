7403ec00849bb0510c805ae72fe6e5f87951218b
Viktor Hermann <v.hermann.it@gmail.com>
2022-08-01 19:42:59 +0200

    gs101: init Ravzen

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -411,5 +411,5 @@ BOARD_KERNEL_CMDLINE += at24.write_timeout=100
 # Enable larger logbuf
 BOARD_KERNEL_CMDLINE += log_buf_len=1024K
 
--include device/google/gs101/BoardConfig-derp.mk
+-include device/google/gs101/BoardConfig-ravzen.mk
 -include vendor/google_devices/gs101/proprietary/BoardConfigVendor.mk
