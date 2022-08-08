c1b4b99a08940301315912a515a478ed73e38955
Viktor Hermann <v.hermann.it@gmail.com>
2022-08-08 21:25:29 +0200

    Revert "sepolicy: use common pixel-sepolicy for turbo_adapter & battery health"
    This reverts commit d0dcf65325220e24dbe1cf6e61fbe11bd088be70.

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -383,8 +383,6 @@ BOARD_BUILD_VENDOR_RAMDISK_IMAGE := true
 
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := device/google/gs101/vendor_dlkm.blocklist
 
-BOARD_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/turbo_adapter
-
 KERNEL_MODULE_DIR := $(TARGET_KERNEL_DIR)
 KERNEL_MODULES := $(wildcard $(KERNEL_MODULE_DIR)/*.ko)
 
